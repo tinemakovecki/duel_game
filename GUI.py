4dfd070d287ea89bff846ea843408f1b2055796e
Tine Makovecki <tine.makovecki@student.fmf.uni-lj.si>
2017-04-10 10:03:47 +0200

    added health bar

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 ### THE USER INTERFACE
 import tkinter
+from tkinter import ttk
 
 root = tkinter.Tk()
 # TODO GET A NAME
@@ -21,40 +22,49 @@ class GUI():
         # creating the buttons for player 1
         # player 1 is at the bottom of the screen
         self.Player1_Button1 = tkinter.Button(self.Game_window, text="attack_1", command=self.attack1)
-        self.Player1_Button1.grid(row=5, column=0)
+        self.Player1_Button1.grid(row=6, column=0, columnspan=2)
 
         self.Player1_Button2 = tkinter.Button(self.Game_window, text="attack_2", command=self.attack2)
-        self.Player1_Button2.grid(row=5, column=1)
+        self.Player1_Button2.grid(row=6, column=2, columnspan=2)
 
         self.Player1_Button3 = tkinter.Button(self.Game_window, text="attack_3", command=self.attack3)
-        self.Player1_Button3.grid(row=6, column=0)
+        self.Player1_Button3.grid(row=7, column=0, columnspan=2)
 
         self.Player1_Button4 = tkinter.Button(self.Game_window, text="attack_4", command=self.attack4)
-        self.Player1_Button4.grid(row=6, column=1)
+        self.Player1_Button4.grid(row=7, column=2, columnspan=2)
 
 
         # creating the buttons for player 2
         # player 2 is at the top of the screen
         self.Player2_Button1 = tkinter.Button(self.Game_window, text="attack_1", command=self.attack5)
-        self.Player2_Button1.grid(row=1, column=0)
+        self.Player2_Button1.grid(row=1, column=0, columnspan=2)
 
         self.Player2_Button2 = tkinter.Button(self.Game_window, text="attack_2", command=self.attack6)
-        self.Player2_Button2.grid(row=1, column=1)
+        self.Player2_Button2.grid(row=1, column=2, columnspan=2)
 
         self.Player2_Button3 = tkinter.Button(self.Game_window, text="attack_3", command=self.attack7)
-        self.Player2_Button3.grid(row=2, column=0)
+        self.Player2_Button3.grid(row=2, column=0, columnspan=2)
 
         self.Player2_Button4 = tkinter.Button(self.Game_window, text="attack_4", command=self.attack8)
-        self.Player2_Button4.grid(row=2, column=1)
+        self.Player2_Button4.grid(row=2, column=2, columnspan=2)
 
 
         # HEALTH BARS AS A WIDGET? - TODO
+        self.Player1_Health_Bar = ttk.Progressbar(self.Game_window, orient="horizontal", length=200, maximum=1000, value=1000)
+        self.Player1_Health_Bar.grid(row=5, column=1, columnspan=2)
+        self.Player1_Health = 1000
+
+        self.Test_Button = tkinter.Button(self.Game_window, text="test_punch", command=self.deal_damage) # for checking if the healthbar works
+        self.Test_Button.grid(row=5, column=3)
+
+
         # creating the game image
         self.Game_picture = tkinter.Canvas(self.Game_window, background='white')
-        self.Game_picture.grid(row=3, rowspan=2, column=0, columnspan=2)
+        self.Game_picture.grid(row=3, rowspan=2, column=0, columnspan=4)
         self.draw_game_picture()
 
 
+        # TODO game menu
 
 
     def draw_game_picture(self):
@@ -97,6 +107,11 @@ class GUI():
     def attack8(self):
         # TODO
         pass
+
+    def deal_damage(self):
+        self.Player1_Health -= 50
+        self.Player1_Health_Bar["value"] = self.Player1_Health
+
 
 
 # TODO window name,...
