b93149d99775e3422c90fc00f96e068f5206cf88
Tine Makovecki <tine.makovecki@gmail.com>
2017-05-10 23:53:41 +0200

    dodan seznam modifierjev

--- Help.py
+++ Help.py
@@ -18,7 +18,7 @@ def help():
 
     ### - FIRST PARAGRAPH - ###
     # introduction
-    tutorial__paragraph_1_text = """
+    tutorial_paragraph_1_text = """
     This game is based on the battles in pokemon games.
     There are two players and each of them controls a
     creature. The monsters have a certain amount of
@@ -27,37 +27,37 @@ def help():
     attack hits it deals damage. You win when the other
     player has no more health left.
     """
-    tutorial__paragraph_1 = tkinter.Label(tutorial_window,
-                                          text=tutorial__paragraph_1_text,
+    tutorial_paragraph_1 = tkinter.Label(tutorial_window,
+                                          text=tutorial_paragraph_1_text,
                                           justify="left")
 
     ### - SECOND PARAGRAPH - ###
     # title
-    tutorial__paragraph_2_title = tkinter.Label(tutorial_window,
+    tutorial_paragraph_2_title = tkinter.Label(tutorial_window,
                                                 text="Attacks:",
                                                 justify="left",
                                                 font=("Purisa", 12))
 
     # paragraph
-    tutorial__paragraph_2_text = """
+    tutorial_paragraph_2_text = """
     Each creature has 4 attacks and every button
     represents an attack. The text written on the
     button is the name, the damage it deals and the
     percentage chance for the attack to hit.
     """
-    tutorial__paragraph_2 = tkinter.Label(tutorial_window,
-                                          text=tutorial__paragraph_2_text,
+    tutorial_paragraph_2 = tkinter.Label(tutorial_window,
+                                          text=tutorial_paragraph_2_text,
                                           justify="left")
 
     ### - THIRD PARAGRAPH - ###
     # title
-    tutorial__paragraph_3_title = tkinter.Label(tutorial_window,
+    tutorial_paragraph_3_title = tkinter.Label(tutorial_window,
                                                 text="Modifiers:",
                                                 justify="left",
                                                 font=("Purisa", 12))
 
     # paragraph
-    tutorial__paragraph_3_text = """
+    tutorial_paragraph_3_text = """
     When an attack hits it also has an extra effect
     besides dealing damage. It affects one of the
     creatures with a "modifier" that changes its
@@ -66,14 +66,34 @@ def help():
     is repeated its effect doesn't stack, instead it
     only refreshes.
     """
-    tutorial__paragraph_3 = tkinter.Label(tutorial_window,
-                                          text=tutorial__paragraph_3_text,
+    tutorial_paragraph_3 = tkinter.Label(tutorial_window,
+                                          text=tutorial_paragraph_3_text,
+                                          justify="left")
+
+    tutorial_mods_text = """
+    Modifier list:
+
+    Pikachu:
+    Lightning: Shock, -10, -10
+    Scratch: Bleed, -10, 0
+    Tackle: Knockdown, -5, -12
+    Paralyze: Paralysis, -10, -25
+
+    Charmander:
+    Fire: Burn, -15, -5
+    Scratch: Bleed, -10, 0
+    Kick: Knockdown: -5, -12
+    Bite: Frenzy, 10, 0
+    """
+    tutorial_mods = tkinter.Label(tutorial_window,
+                                          text=tutorial_mods_text,
                                           justify="left")
 
     # pack the text elements in the right order
     tutorial_title.pack()
-    tutorial__paragraph_1.pack()
-    tutorial__paragraph_2_title.pack()
-    tutorial__paragraph_2.pack()
-    tutorial__paragraph_3_title.pack()
-    tutorial__paragraph_3.pack()
+    tutorial_paragraph_1.pack()
+    tutorial_paragraph_2_title.pack()
+    tutorial_paragraph_2.pack()
+    tutorial_paragraph_3_title.pack()
+    tutorial_paragraph_3.pack()
+    tutorial_mods.pack()
